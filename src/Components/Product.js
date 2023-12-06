9b7d2fa658664a2f226c5e18833c4cfc4ed30a2c
Swapnil bhakare <swapnilbhakare7@gmail.com>
2023-12-06 19:34:50 +0530

    attendance

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = (props) => {
+  const submitHandler = () => {
+    console.log("abc");
+  };
+  return (
+    <div className="product">
+      <h3>{props.title}</h3>
+      <h3>{props.price}</h3>
+      <button onClick={submitHandler}>Click</button>
+    </div>
+  );
 };
 
 export default Product;
--- src/Components/Products_List.js
+++ src/Components/Products_List.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Product from "./Product";
 
 const Products_List = () => {
-  return <div>Products_List</div>;
+  return (
+    <div className="products">
+      <Product title="iphone 12" price="200" />
+      <Product title="macbook" price="5500" />
+      <Product title="redmi 12" price="50" />
+    </div>
+  );
 };
 
 export default Products_List;
